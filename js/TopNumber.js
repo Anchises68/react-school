6903587821d4f6cf42a9ec28f7c20c87d42a4eed
Alex Mireles <anchises1968@gmail.com>
2018-05-02 16:13:48 -0700

    more exercises

---
+++ js/TopNumber.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const yellow = 'rgb(255, 215, 18)';
+import PropTypes from 'prop-types';
+
+export class TopNumber extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 'highest': 0 };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.number > this.state.highest) {
+      this.setState({
+        highest: nextProps.number
+      });
+    }
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (document.body.style.background != yellow && this.state.highest >= 950*1000) {
+      document.body.style.background = yellow;
+   }
+  }
+
+  render() {
+    return (
+      <h1>
+        Top Number: {this.state.highest}
+      </h1>
+    );
+  }
+}
+
+TopNumber.propTypes = {
+  number: React.PropTypes.number,
+  game: React.PropTypes.bool
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/containers/GuineaPigsContainer.js',
+  entry: './js/App.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
